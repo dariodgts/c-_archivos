f9396b260c9f10fa96d3d1abd6f2ff013aa2e40a
dariodgts <dariogregoriotejeda@gmail.com>
2022-09-09 20:54:30 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Base1, Altura, Area, Perimetro;
+            
+            Console.WriteLine(" Digite la Base:");
+            Base1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite la Altura:");
+           Altura = Double.Parse(Console.ReadLine());
+
+            Area = Base1 * Altura / 2;
+            Perimetro = Base1 * 3;
+          
+
+            Console.WriteLine("\n\nSU AREA ES: "+Area);
+            Console.WriteLine("SU PERIMETRO ES: "+Perimetro);
+            Console.ReadKey();
+        }
+    }
+}
